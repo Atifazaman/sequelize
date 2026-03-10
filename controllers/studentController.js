1a1c920ed8549b05ebe6871eaf541fad97e4d4a9
Atifazaman <136952774+Atifazaman@users.noreply.github.com>
2026-03-10 11:48:19 +0530

    Update studentController.js

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,88 +1,92 @@
 const db=require('../utils/dbConnection')
+const Student=require('../models/students')
 
-const getEntries=(req,res)=>{
-const getQuery=`select * from students`
-db.execute(getQuery,(err,result)=>{
-if(err){
-    console.log(err.message)
-    res.status(500).send(err.message)
-    db.end()
-    return
+
+const getEntries=async(req,res)=>{
+try{
+const student=await Student.findAll()
+res.status(200).json(student)
+}catch(err){
+    res.status(500).send("Unable to retrieve students details")
 }
-if(result.length===0){
-    res.status(404).send("No Students Stored in database")
-}
-res.status(200).send(result)
-})
 }
 
 
-const getEntryById=(req,res)=>{
-    const {id}=req.params
-const getByIdQuery=`select * from students where id=?`
-db.execute(getByIdQuery,[id],(err,result)=>{
-if(err){
-    console.log(err.message)
-    res.status(500).send(err.message)
-    db.end()
-    return
-}
-if(result.length===0){
-    res.status(404).send("Student Not Found")
-}
-res.status(200).send(result)
-})
-}
 
-const addEntries=(req,res)=>{
-    const {name,email,age}=req.body
-const addStudentQuery=`insert into students(name,email,age)values(?,?,?)`
-db.execute(addStudentQuery,[name,email,age],(err)=>{
-if(err){
-    console.log(err.message)
-    res.status(500).send(err.message)
-    db.end()
-    return
-}
-res.status(200).send("Student stored successfully")
-})
-}
-
-const updateEntry=(req,res)=>{
-   const {name,email}=req.body
-   const {id}=req.params
-const updateStudentQuery=`update students set name=?,email=? where id=?`
-db.execute(updateStudentQuery,[name,email,id],(err,result)=>{
-if(err){
-    console.log(err.message)
-    res.status(500).send(err.message)
-    db.end()
-    return
-}
-if(result.affectedRows===0){
-    res.status(404).send("Student Not Found")
-    return
-}
-res.status(200).send("Student updated successfully")
-})
-}
-
-const deleteEntry=(req,res)=>{
+const getEntryById=async(req,res)=>{
+   
+try{
  const {id}=req.params
-const deleteStudentQuery=`delete from students where id=?`
-db.execute(deleteStudentQuery,[id],(err,result)=>{
-if(err){
-    console.log(err.message)
-    res.status(500).send(err.message)
-    db.end()
-    return
+ const student=await Student.findByPk(id)
+ if(!student){
+     res.status(404).send("Student Not Found")
+ }
+ res.status(200).json(student)
+}catch(err){
+     res.status(500).send("Unable to retrieve student details")
 }
-if(result.affectedRows===0){
-    res.status(404).send("Student Not Found")
-    return
 }
-res.status(200).send("Student deleted successfully")
-})
+
+
+
+const addEntries=async (req,res)=>{
+try{
+    const {name,email,age}=req.body
+    const student=await Student.create({
+        name:name,
+        email:email,
+        age:age
+    })
+res.status(201).send("Student stored successfully")
+}
+catch(err){
+    console.log(err)
+res.status(500).send("unable to make an entry")
+}
+}
+
+
+const updateEntry=async(req,res)=>{ 
+  try{
+    const {name,email,age}=req.body
+ const {id}=req.params
+ const student=await Student.findByPk(id)
+
+ if(!student){
+     res.status(404).send("Student Not Found")
+ }
+
+ student.name = name
+ student.email=email
+student.age = age
+
+await student.save()
+
+ res.status(200).json(student)
+}catch(err){
+     res.status(500).send("Unable to update student details")
+}
+}
+
+
+
+const deleteEntry=async(req,res)=>{
+ try{
+ const {id}=req.params
+ const student=await Student.destroy({
+    where:{
+        id:id
+    }
+ })
+ 
+ if(!student){
+    return res.status(404).send("Student Not Found")
+ }
+
+ res.status(200).send("Student has been deleted")
+}catch(err){
+     res.status(500).send("Unable to delete student details")
+}
 }
 
 module.exports={
