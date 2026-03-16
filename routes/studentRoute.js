6979b65a5bd55808354980ca08f072ef19202488
Atifazaman <136952774+Atifazaman@users.noreply.github.com>
2026-03-16 12:54:10 +0530

    Update studentRoute.js

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -8,6 +8,7 @@ studentRoute.get("/:id",studentController.getEntryById)
 studentRoute.post("/",studentController.addEntries)
 studentRoute.put("/:id",studentController.updateEntry)
 studentRoute.delete("/:id",studentController.deleteEntry)
-
+studentRoute.post("/addingWithDepartment",studentController.createStudentWithDepartment)
+studentRoute.get("getWithDepartment",studentController.getDepartmentWithStudents)
 
 module.exports=studentRoute
